0dc03213c92c6f889c82704f38e9f8e3a57e1fe5
sumanta-c <73934724+sumanta-c@users.noreply.github.com>
2021-12-07 19:43:42 +0530

    Update CalcTest.cs

--- DemoCoreApp.Tests/CalcTest.cs
+++ DemoCoreApp.Tests/CalcTest.cs
@@ -19,6 +19,7 @@ namespace DemoCoreApp.Tests
         [InlineData(10,10,20)]
         [InlineData(13,14,27)]
         [InlineData(-1,-1,-2)]
+        [InlineData(2,3,5)]
         public void Add_TestPass(double op1, double op2, double res)
         {
             calc objcalc = new calc();  
@@ -31,6 +32,7 @@ namespace DemoCoreApp.Tests
         [InlineData(30,10,20)]
         [InlineData(43,24,19)]
         [InlineData(-1,-7,6)]
+        [InlineData(3,2,1)]
         public void Subtract_TestPass(double op1, double op2, double res)
         {
             calc objcalc = new calc();  
